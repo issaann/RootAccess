55b615445b36c67ccd9ffca0659a5db325bed95a
issaann <155139278+issaann@users.noreply.github.com>
2024-02-19 18:29:28 +0530

    Update index.js

--- bitByBit/index.js
+++ bitByBit/index.js
@@ -1,2 +1,31 @@
- const lovish=require("./second");
- console.log("i am good",lovish)
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 4000;
+
+// Assuming your HTML file is in the 'hackthon' folder
+const hackthonPath = path.join(__dirname, 'hackthon');
+app.use(express.static(hackthonPath));
+
+// Route for the root URL
+app.get('/', (req, res) => {
+    const htmlFilePath = path.join(hackthonPath, 'C:\Users\issaa\OneDrive\Desktop\hackathon\business.html');
+
+    // Check if the file exists before attempting to send it
+    if (require('fs').existsSync("C:\Users\pc\Desktop\hackthon\index.html")) {
+        res.sendFile("C:\Users\issaa\OneDrive\Desktop\hackathon\business.html");
+    } else {
+        res.status(404).send('File not found');
+    }
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.listen(port, () => {
+   // console.log("server is running on port"${port});
+});
